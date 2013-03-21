36898aa0a645f67e76431eee3cca65ddc95c7675
Kevin Quan <ksquan@gmail.com>
2013-03-21 14:41:55 -0700

    subscription bug update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree ../../../vendor/assets/javascripts/.
-//= require autocomplete-rails
 
 
 $(function() {
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Gift::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
